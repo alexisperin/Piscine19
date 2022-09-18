c65ec800de2f17b32c169b6bd312b56475398933
Alexis Perin <alexis.perin@hotmail.com>
2022-09-18 12:12:54 +0200

    Add files via upload

---
+++ Rush01/map.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/17 12:54:51 by aperin            #+#    #+#             */
+/*   Updated: 2022/09/17 14:06:01 by aperin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int	*next_pos(int *pos)
+{
+	if (pos[1] < 3)
+		pos[1]++;
+	else if (pos[0] < 3)
+	{
+		pos[0]++;
+		pos[1] = 0;
+	}
+	else
+	{
+		pos[0] = -1;
+		pos[1] = -1;
+	}
+	return (pos);
+}
+
+int	*prev_pos(int *pos)
+{
+	if (pos[1] > 0)
+		pos[1]--;
+	else
+	{
+		pos[0]--;
+		pos[1] = 3;
+	}
+	return (pos);
+}
+
+void	init_map(int ***map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			(*map)[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+}
+
+int	**build_map(void)
+{
+	int	**map;
+	int	i;
+
+	i = 0;
+	map = (int**)malloc(4 * sizeof(int*));
+	if (map == 0)
+		return (0);
+	while (i < 4)
+	{
+		map[i] = (int*)malloc(4 * sizeof(int));
+		if (map[i] == 0)
+			return (0);
+		i++;
+	}
+	init_map(&map);
+	return (map);
+}
+
+void	print_map(int **map)
+{
+	int		i;
+	int		j;
+	char	c;
+
+	i = 0;
+	j = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			c = map[i][j] + 48;
+			write(1, &c, 1);
+			if (j != 3)
+				write(1, " ", 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
---
+++ Rush01/solve.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aperin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/17 12:41:27 by aperin            #+#    #+#             */
+/*   Updated: 2022/09/17 14:34:30 by aperin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush01.h"
+#include <stdlib.h>
+#include <stdio.h>
+/*
+void	print_tab(int *tab)
+{
+	int	i;
+
+	i = 0;
+	while (i < 16)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	printf("\n");
+}
+*/
+void	extract_input(char *input, int **view)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < 31)
+	{
+		(*view)[j] = input[i] - 48;
+		i += 2;
+		j++;
+	}
+}
+
+int backtrack(int ***map, int *views, int *pos)
+{
+	if (pos[0] == -1 && pos[1] == -1)
+	{
+		if (solved(*map, views))
+			return (1);
+		else
+			return (backtrack(map, views, prev_pos(pos)));
+	}
+	else if ((*map)[pos[0]][pos[1]] < 4)
+	{
+		(*map)[pos[0]][pos[1]] += 1;
+		if (duplicate(*map, pos))
+			return (backtrack(map, pos));
+		return (backtrack(map, views, next_pos(pos)));
+	}
+	else if (!(pos[0] == 0 && pos[1] == 0))
+	{
+		(*map)[pos[0]][pos[1]] = 0;
+		return (backtrack(map, views, prev_pos(pos)));
+	}
+	else
+		return (0);
+}
+
+int	solve_game(char *input)
+{
+	int	**map;
+	int	*views;
+	int start[2];
+
+	map = build_map();
+	views = malloc(16 * sizeof(int));
+	if (views == 0)
+		return (0);
+	extract_input(input, &views);
+	start[0] = 0;
+	start[1] = 0;
+	if (!backtrack(&map, views, start))
+		return (0);
+	print_map(map);
+	//print_tab(views);
+	return (1);
+}
