a904341dc94a3d134e4c75d8008bc0a6ce1bb8bc
Alexis Perin <alexis.perin@hotmail.com>
2022-09-21 15:19:15 +0200

    Add ex02

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -1,21 +1,84 @@
 #include <stdlib.h>
 
-char **ft_split(char *str, char *charset)
+int	is_seperator(char c, char *charset)
 {
-	char **strs;
-	int size;
-	int i;
-	int j;
+	int	i;
 
-	size = count_strings(str, charset) + 1;
-	strs = malloc(size * sizeof(char *));
-	if (strs == NULL)
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+
+int	size_split(char *str, char *charset)
+{
+	int	i;
+	int	size;
+
+	i = 0;
+	size = 0;
+	while (str[i])
+	{
+		while (str[i] && is_seperator(str[i], charset))
+			i++;
+		if (str[i])
+			size++;
+		while (str[i] && !is_seperator(str[i], charset))
+			i++;
+	}
+	return (size);
+}
+
+char	*get_substring(char *str, int start, char *charset)
+{
+	int		len;
+	int		i;
+	char	*substring;
+
+	len = 0;
+	while (str[start + len] && !is_seperator(str[start + len], charset))
+		len++;
+	substring = malloc((len + 1) * sizeof(char));
+	if (substring == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substring[i] = str[start + i];
+		i++;
+	}
+	substring[i] = '\0';
+	return (substring);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**split;
+	int		i;
+	int		j;
+
+	split = malloc((size_split(str, charset) + 1) * sizeof(char *));
+	if (split == NULL)
 		return (NULL);
 	i = 0;
 	j = 0;
 	while (str[i])
 	{
-		
+		while (str[i] && is_seperator(str[i], charset))
+			i++;
+		if (str[i])
+		{
+			split[j] = get_substring(str, i, charset);
+			j++;
+		}
+		while (str[i] && !is_seperator(str[i], charset))
+			i++;
 	}
-
+	split[j] = 0;
+	return (split);
 }
